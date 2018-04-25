c93807ea0a28d2951235babf49fe08899aa59e8d
Pico143 <pico19942@gmail.com>
2018-04-25 09:19:55 +0200

    added first site template

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home_page():
+    return redirect('/question-with-one-answer')
+
+@app.route('/question-with-one-answer')
+def question_with_one_answer():
+    return render_template('question.html')
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8000,
+        debug=True,
+    )
